66ebbd72df5c3b30249cf9de691741ebf584b49b
Caglar <caglarsulun@hotmail.com>
2021-11-24 00:50:20 -0500

    commit tree fd80ff5339cace73453f23446c652df3a3fc89ef

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
+
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
+saturdayFun();
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+
+function mondayWork(activity = 'go to the office'){
+  return `This Monday, I will ${activity}.`;
 }
+mondayWork();
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+
+let wrapAdjective = function(highlighter="*") {
+   return function adj (adjective="special"){
+    return `You are ${highlighter}${adjective}${highlighter}!`
   }
+ 
+  
 }
+wrapAdjective();
+
+
+
+
+
+
+
+// let wrapAdjective = function(highlighter="*") {
+//   return function (adjective="special") {
+//     return `You are ${highlighter}${adjective}${highlighter}!`
+//   }
+// }
+// wrapAdjective();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun(activity="roller-skate") {
+//   return `This Saturday, I want to ${activity}!`
+// }
+
+// let mondayWork = function(activity="go to the office") {
+//   return `This Monday, I will ${activity}.`
+// }
+
+// let wrapAdjective = function(style="*") {
+//   return function(adjective="special") {
+//     return `You are ${style}${adjective}${style}!`
+//   }
+// }
